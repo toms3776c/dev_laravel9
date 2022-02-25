737225d7122e06338bf7d0fd5cad4950613c3421
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-02-26 07:33:36 +0900

    Tailwind CSS、デバッガーを追加

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Articles</title>
 </head>
 <body>
-    <h1>記事一覧</h1>
+    <h1 class="text-2xl font-bold text-blue-600">記事一覧</h1>
 </body>
 </html>
