3ff8e85e3d5162a34c4c9244900323b9caf201ee
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-03-21 16:32:21 +0900

    daisyUIの適用。Qiita取得データの整形表示対応。

--- resources/views/qiita/index.blade.php
+++ resources/views/qiita/index.blade.php
@@ -11,15 +11,20 @@
     <h1 class="text-3xl font-bold text-blue-600">Qiita投稿記事</h1>
     <ul>
         @foreach ($decode_res as $res_data)
-        <ul>
-            <li>
-                {{-- {!! print_r($res_data, true) !!} --}}
-
-                {{ $res_data->title }}
-            </li>
-        </ul>
+            <div class="card w-3/4 m-4 shadow-lg">
+                <div class="card-body">
+                    <h2 class="card=title text-xl">{{ $res_data->title }}</h2>
+                    <div class="flex">
+                        @foreach ($res_data->tags as $tag)
+                            <div class="badge badge-secondary">{{ $tag->name }}</div>
+                        @endforeach
+                    </div>
+                    <div class="card-actions justify-end">
+                        <a class="btn btn-primary" href="{{ $res_data->url }}" target="_blank">公開サイトへ</a>
+                    </div>
+                </div>
+            </div>
         @endforeach
     </ul>
-
 </body>
 </html>
