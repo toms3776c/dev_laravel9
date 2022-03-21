aef3b14c650e8b2ea58dce0545b2dbdb63002c50
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-03-21 17:24:19 +0900

    画面要素を中央寄せに

--- resources/views/qiita/index.blade.php
+++ resources/views/qiita/index.blade.php
@@ -8,15 +8,15 @@
     <title>Articles</title>
 </head>
 <body>
-    <h1 class="text-3xl font-bold text-blue-600">Qiita投稿記事</h1>
-    <ul>
+    <div class="container mx-auto bg-base-200">
+        <h1 class="text-4xl font-bold text-blue-600 text-center">Qiita記事</h1>
         @foreach ($decode_res as $res_data)
-            <div class="card w-3/4 m-4 shadow-lg">
+            <div class="card m-4 w-3/4 bg-base-100 mx-auto shadow-lg">
                 <div class="card-body">
                     <h2 class="card=title text-xl">{{ $res_data->title }}</h2>
                     <div class="flex">
                         @foreach ($res_data->tags as $tag)
-                            <div class="badge badge-secondary">{{ $tag->name }}</div>
+                            <div class="badge badge-secondary m-1">{{ $tag->name }}</div>
                         @endforeach
                     </div>
                     <div class="card-actions justify-end">
@@ -25,6 +25,6 @@
                 </div>
             </div>
         @endforeach
-    </ul>
+    </div>
 </body>
 </html>
